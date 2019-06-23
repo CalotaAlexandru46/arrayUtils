bed66897562c80cb11fef81b7db0a9d67999a579
Calota Alexandru <calotaalexandrug@gmail.com>
2019-06-23 12:52:11 +0300

    Initial commit

---
+++ src/test/java/com/company/ArraysUtilsTest.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArraysUtilsTest {
+
+    @Test
+    public void binarySearchTest(){
+        int[] array={1,3,5,7,9,10};
+        int n=7;
+        int result=new ArraysUtils().binarySearch(array,n);
+        Assert.assertEquals(3, result);
+    }
+
+    @Test
+    public void binarySearchTest2(){
+        int[] array={1,3,5,7,9,10};
+        int n=4;
+        int result=new ArraysUtils().binarySearch(array,n);
+        Assert.assertEquals(-3,result);
+    }
+
+    @Test
+    public void bubbleSortTest(){
+        int[] arr={4,5,2,6,1,3};
+        new ArraysUtils().bubbleSort(arr);
+        Assert.assertArrayEquals(new int[]{1,2,3,4,5,6},arr);
+    }
+
+
+
+
+
+}
